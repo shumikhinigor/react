df18ab48af462242e139b4244aa702d1efc177b3
Igor Shumikhin <dev.shumikhin@gmail.com>
2022-04-20 16:55:37 +0300

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends React.Component {
 		return (
 			<div className="App">
 				<header className="App-header">
-					<Welcome name={'Igor'} />
+					<Welcome />
 					<Clock />
 					<img
 						src={ logo }
