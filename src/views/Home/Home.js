3995408ec32348994ea79fe51a0bbec0fb66cf86
Igor Shumikhin <dev.shumikhin@gmail.com>
2022-04-23 16:02:54 +0300

    refactoring styles

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,11 +1,17 @@
 import { Component } from 'react'
 import classes from './Home.module.sass'
 import Welcome from '../../components/Welcome/Welcome'
+import logo from '../../logo.svg'
 
 class Home extends Component {
 	render() {
 		return (
 			<div className={ classes.Home }>
+				<img
+					src={ logo }
+					className={ classes['Home-logo'] }
+					alt="logo"
+				/>
 				<Welcome />
 			</div>
 		)
