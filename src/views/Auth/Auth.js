4d919139c2e3bbf51d86053630829b87da6aa217
Igor Shumikhin <dev.shumikhin@gmail.com>
2022-04-23 17:35:21 +0300

    styles for auth

--- src/views/Auth/Auth.js
+++ src/views/Auth/Auth.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react'
 import classes from './Auth.module.sass'
+import Button from '../../components/UI/Button/Button'
 
 class Auth extends Component {
+
+	onLoginHandler = () => {
+		console.log('loginHandler')
+	}
+	onRegisterHandler = () => {
+		console.log('registerHandler')
+	}
+	onSubmitHandler = event => {
+		console.log('onSubmitHandler')
+		event.preventDefault()
+	}
+
 	render() {
 		return (
 			<div className={ classes.Auth }>
-				<h1>Auth</h1>
+				<div>
+					<h1>Authorization</h1>
+
+					<form
+						className={ classes['Auth-form'] }
+						onSubmit={ this.onSubmitHandler }
+					>
+						<input type="text" />
+						<input type="text" />
+
+						<Button
+							type={ 'success' }
+							onClick={ this.onLoginHandler }
+						>Log in</Button>
+
+						<Button
+							type={ 'primary' }
+							onClick={ this.onRegisterHandler }
+						>Register</Button>
+					</form>
+				</div>
 			</div>
 		)
 	}
